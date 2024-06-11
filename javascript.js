4ac9e14690ab8b5cead4a1cf02e662a9faea2c88
makimakimek <melodiezgi17@gmail.com>
2024-06-11 12:17:03 +0300

    the value being clicked is displayed on calculator now

--- javascript.js
+++ javascript.js
@@ -17,3 +17,21 @@ function divide(num1, num2) {
 function operate(num1, operator, num2) {
     add(num1, num2);
 }
+
+function populateDisplay() {
+    const numbersAndOperators = document.querySelectorAll('.subElement');
+    const resultScreen = document.querySelector('.resultScreen');
+
+    let displayValue = 0;
+
+    numbersAndOperators.forEach((numberOrOperator) => {
+        numberOrOperator.addEventListener("click", () => {
+            displayValue = numberOrOperator.textContent;
+            resultScreen.innerHTML += numberOrOperator.textContent;
+        });
+    });
+
+    return displayValue;
+}
+
+populateDisplay();
